dadb50cb4e6c9b5eb0aad86ca23ba9a0f26a8cc6
ekicyou <dot.station@gmail.com>
2023-04-11 13:30:30 +0900

    現状でコミット

--- crates/pest_parser/tests/simple_test.rs
+++ crates/pest_parser/tests/simple_test.rs
@@ -1,6 +1,48 @@
 use pasta_parser_pest::*;
 
 #[test]
+fn scene4_line() {
+    let rule = Rule::scene4_line;
+    let input = "：：：：：シーン４　属性１＠１０　￥￥こめんと";
+    {
+        let pairs = PastaParser::parse(rule, input).unwrap_or_else(|e| panic!("{}", e));
+        println!("pairs:\n{}\n", pairs);
+    }
+    parses_to! {
+        parser: PastaParser,
+        input: "：：：：：シーン４　属性１＠１０　￥￥こめんと",
+        rule: Rule::scene4_line,
+        tokens: [scene4_line(0, 69, [id_attr(15, 48, [id(15, 27), attrs(27, 48, [attr(30, 48, [id(30, 39), expr(42, 48, [num(42, 48, [NUM1(42, 45), NUM0(45, 48)])])])])]), comment(51, 69, [comment_word(57, 69)]), EOI(69, 69)])]
+    };
+}
+
+#[test]
+fn scene1_line() {
+    let rule = Rule::scene1_line;
+    let input = "：：シーン１　属性１＠１０　￥￥こめんと";
+    {
+        let pairs = PastaParser::parse(rule, input).unwrap_or_else(|e| panic!("{}", e));
+        println!("pairs:\n{}\n", pairs);
+    }
+    parses_to! {
+        parser: PastaParser,
+        input: "：：シーン１　属性１＠１０　￥￥こめんと",
+        rule: Rule::scene1_line,
+        tokens: [scene1_line(0, 60, [id_attr(6, 39, [id(6, 18), attrs(18, 39, [attr(21, 39, [id(21, 30), expr(33, 39, [num(33, 39, [NUM1(33, 36), NUM0(36, 39)])])])])]), comment(42, 60, [comment_word(48, 60)]), EOI(60, 60)])]
+    };
+}
+
+#[test]
+fn scene1_mark() {
+    let rule = Rule::SCENE1;
+    let input = "::";
+    {
+        let pairs = PastaParser::parse(rule, input).unwrap_or_else(|e| panic!("{}", e));
+        println!("pairs:{}", pairs);
+    }
+}
+
+#[test]
 fn id_attr_test() {
     parses_to! {
         parser: PastaParser,
@@ -16,7 +58,7 @@ fn id_attr_test() {
     {
         let input = "abc x@10 y@str";
         let pairs = PastaParser::parse(Rule::id_attr, input).unwrap_or_else(|e| panic!("{}", e));
-        println!("pairs:{}", pairs);
+        println!("pairs:\n{}\n", pairs);
     }
     parses_to! {
         parser: PastaParser,
@@ -43,13 +85,16 @@ fn id_attr_test() {
 
 #[test]
 fn parses_to_test() {
+    {
+        let input = "//comment";
+        let pairs = PastaParser::parse(Rule::comment, input).unwrap_or_else(|e| panic!("{}", e));
+        println!("pairs:\n{}\n", pairs);
+    }
     parses_to! {
         parser: PastaParser,
         input: "//comment",
         rule: Rule::comment,
-        tokens: [
-            comment(0, 9, []),
-        ]
+        tokens: [comment(0, 9, [comment_word(2, 9)])]
     };
 }
 
